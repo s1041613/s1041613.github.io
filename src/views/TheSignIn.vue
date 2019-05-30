27e0264432020dccd4c44750b2ec55cf5cd7f4c4
jennychou <y10135124@gmail.com>
2019-05-30 23:52:06 +0800

    google auth

--- src/views/TheSignIn.vue
+++ src/views/TheSignIn.vue
@@ -48,47 +48,23 @@ export default {
   
   },
   methods: {
+    
     logIn() {
       Vue.googleAuth().directAccess()
       Vue.googleAuth().signIn(this.onSignInSuccess, this.onSignInError) 
     },
-     onSignInSuccess: function (authorizationCode) {
-      console.log('onSignInSuccess')
-      this.toggleLoading()
-      this.resetResponse()
-      this.$http.post('http://your-backend-server.com/auth/google', { code: authorizationCode, redirect_uri: 'postmessage' }).then(function (response) {
-        // if (response.body) {
-        //   var data = response.body
-        //   // Save to vuex
-        //   var token = 'Bearer ' + data.token
-        //   this.$store.commit('SET_USER', data.user_data)
-        //   this.$store.commit('SET_TOKEN', token)
-        //   // Save to local storage as well 
-        //   // ( or you can install the vuex-persistedstate plugin so that you won't have to do this step, only store to Vuex is sufficient )
-        //   if (window.localStorage) {
-        //     window.localStorage.setItem('user', JSON.stringify(data.user_data))
-        //     window.localStorage.setItem('token', token)
-        //   }
-          // redirect to the dashboard
-          this.$router.push({ name: '/home' })
-        // }
-      }, function (response) {
-        var data = response.body
-        this.response = data.error
-        console.log('BACKEND SERVER - SIGN-IN ERROR', data)
-      })
+    onSignInSuccess: function(googleUser) {
+      let vm=this;
+      localStorage.setItem("signinStatus", true)
+      var prof = googleUser.getBasicProfile()
+      vm.signinEmail = prof.getEmail()
+      localStorage.setItem("signinEmail", vm.signinEmail) //localstroage remembers user is signed in
+      this.$router.push({path:'/home'})
     },
     onSignInError(error) {
       this.response = 'Failed to sign-in'
       console.log('status',this.response)
     },
-    toggleLoading: function () {
-      this.loading = (this.loading === '') ? 'loading' : ''
-    },
-    resetResponse: function () {
-      this.response = ''
-    }
-
   },
   mounted() {
 
