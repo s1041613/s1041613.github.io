0fb8eb7749862c62bb53d67ff981fbbe5ef1681f
jennychou <y10135124@gmail.com>
2019-06-04 20:21:45 +0800

    ./

--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,7 @@
 // }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/s1041613.github.io/' : '/',
-  // publicPath:'./',
+  publicPath:'./',
   outputDir:'dist',//打包的时候生成的一个文件名
   assetsDir:'assets',//静态资源目录(js,css,img,fonts)这些文件都可以写里面
   lintOnSave:false,//是否开启eslint保存检测 ,它的有效值为 true || false || 'error'
